6ea118700131f0b3116cfcd3637f3907e9826188
James Cassidy <cassidyjamesw@gmail.com>
2019-05-22 18:09:34 -0400

    cleaned pages

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -2,9 +2,7 @@ import Link from "next/link";
 
 const Home = props => (
   <div>
-    <Link href="/sell">
-      <a>Sell!</a>
-    </Link>
+    <p>Hey!</p>
   </div>
 );
 
--- frontend/pages/sell.js
+++ frontend/pages/sell.js
@@ -2,9 +2,7 @@ import Link from "next/link";
 
 const Sell = props => (
   <div>
-    <Link href="./index">
-      <a>Home</a>
-    </Link>
+    <p>Sell!</p>
   </div>
 );
 

c5bd33b39947d76bee118c130e26e750e8a30e42
James Cassidy <cassidyjamesw@gmail.com>
2019-05-22 18:02:15 -0400

    routing and custom appjs

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,6 +1,10 @@
+import Link from "next/link";
+
 const Home = props => (
   <div>
-    <p>Hey!</p>
+    <Link href="/sell">
+      <a>Sell!</a>
+    </Link>
   </div>
 );
 
--- frontend/pages/sell.js
+++ frontend/pages/sell.js
@@ -1,6 +1,10 @@
+import Link from "next/link";
+
 const Sell = props => (
   <div>
-    <p>Hey!</p>
+    <Link href="./index">
+      <a>Home</a>
+    </Link>
   </div>
 );
 
